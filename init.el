24fe6e2e8897d89c5018fff9dba92239f0e20d68
soma_arc <somaarcr@gmail.com>
2016-11-29 22:08:03 +0900

    Add functions for unfilling

--- init.el
+++ init.el
@@ -317,3 +317,21 @@
   (package-install 'clj-refactor))
 (add-hook 'cider-mode-hook 'clj-refactor-mode)
 
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+;;; https://www.emacswiki.org/emacs/UnfillParagraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+;;; https://www.emacswiki.org/emacs/UnfillRegion
+(defun unfill-region (beg end)
+  "Unfill the region, joining text paragraphs into a single
+    logical line.  This is useful, e.g., for use with
+    `visual-line-mode'."
+  (interactive "*r")
+  (let ((fill-column (point-max)))
+    (fill-region beg end)))
