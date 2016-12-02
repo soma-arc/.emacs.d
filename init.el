1abead6bce8793151527dfd80ff28c96fed9619d
soma_arc <somaarcr@gmail.com>
2016-12-02 10:44:05 +0900

    Use word-diff

--- init.el
+++ init.el
@@ -335,3 +335,22 @@
   (interactive "*r")
   (let ((fill-column (point-max)))
     (fill-region beg end)))
+
+
+
+;; color http://www.clear-code.com/blog/2012/4/3.html
+(defun diff-mode-setup-faces ()
+  (set-face-attribute 'diff-added nil
+                      :foreground "white" :background "dark green")
+  (set-face-attribute 'diff-removed nil
+                      :foreground "white" :background "dark red")
+  (set-face-attribute 'diff-refine-change nil
+                      :foreground nil :background nil
+                      :weight 'bold :inverse-video t))
+
+(defun diff-mode-refine-automatically ()
+  (diff-auto-refine-mode t))
+(add-hook 'diff-mode-hook 'diff-mode-refine-automatically)
+
+(setq magit-diff-refine-hunk 'all)
+(set-face-attribute 'magit-item-highlight nil :inherit nil)
